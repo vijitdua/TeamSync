d0f13ce7a4b6232923fdb18d119ce362e2d089b2
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-08-18 17:09:56 -0700

    feat: Implement CRUD controllers for team data

--- backend/src/controller/teamDataController.js
+++ backend/src/controller/teamDataController.js
@@ -1,39 +1,95 @@
-export function getAllTeamsController(req, res) {
+import {
+    getAllTeams, 
+    getTeam,
+    addTeam,
+    updateTeam,
+    deleteTeam
+} from "../services/teamDataService"
+
+export async function getAllTeamsController(req, res) {
     try{
+        const teams = await getAllTeams();  // Fetch all teams
 
+        res.status(200).json({
+            success: true,
+            data: teams,
+        });
+        
     }catch(err){
-
+        res.status(400).json({
+            success: false, 
+            error: err.message, 
+        })
     }
 }
 
-export function createTeamController(req, res){
+export async function createTeamController(req, res){
     try{
+        const newTeam = await addTeam(req.body);  // Use the request body data to create a new team
 
+        res.status(200).json({
+            success: true, 
+            data: newTeam,
+        })
     }catch(err){
-
+        res.status(400).json({
+            success: false, 
+            message: err.message, 
+        });
     }
 }
 
-export function getTeamController(req, res){
+export async function getTeamController(req, res){
     try{
+        const id = req.params.id;  // Get team ID from request parameters
+        const team = getTeam(id);
 
+        if (!team) {
+            throw Error("Team not found");
+        }
+
+        res.status(200).json({
+            success: true, 
+            data: team,
+        })
     }catch(err){
-
+        res.status(400).json({
+            success: false, 
+            message: err.message, 
+        });
     }
 }
 
-export function updateTeamController(req, res){
+export async function updateTeamController(req, res){
     try{
+        const id = req.params.id;  // Get team ID from request parameters
+        const team = updateTeam(id, req.body);
 
+        res.status(200).json({
+            success: true, 
+            data: team,
+        })
     }catch(err){
-
+        res.status(400).json({
+            success: false, 
+            message: err.message, 
+        });
     }
 }
 
-export function deleteTeamController(req, res){
+export async function deleteTeamController(req, res){
     try{
-
+        const id = req.params.id;  // Get team ID from request parameters
+        const deleted = await deleteTeam(id);  // Delete the team
+        
+        res.status(200).json({
+            success: true, 
+            data: deleted,
+        })
     }catch(err){
-
+        res.status(400).json({
+            success: false, 
+            message: err.message, 
+        });
     }
 }

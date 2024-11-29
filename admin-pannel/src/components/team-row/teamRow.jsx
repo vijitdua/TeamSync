67366b6ded0add432fbe3e3c30c948767d9a87f7
naomitzhao <116607148+naomitzhao@users.noreply.github.com>
2024-11-28 16:13:20 -0800

    feat: Basic Teams layout

---
+++ admin-pannel/src/components/team-row/teamRow.jsx
@@ -0,0 +1,62 @@
+import { Box, Checkbox, Grid2, Paper, Typography, useTheme } from "@mui/material";
+import { useState } from "react";
+
+
+function TeamRow({team, onToggleSelect}) {
+    const theme = useTheme();
+
+    const [isSelected, setIsSelected] = useState(false);
+
+    function toggleSelect() {
+        setIsSelected(!isSelected);
+        onToggleSelect();
+    }
+
+    return (
+        <Paper variant="outlined" sx={{
+            backgroundColor: isSelected ? "#e9eeff" : "#d6ddf9",
+        }}>
+            <Grid2 container spacing={2} sx={{
+                alignItems: "center",
+            }}>
+                {/* Checkbox */}
+                <Grid2 size={0.5} sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                }}>
+                    <Checkbox onChange={ toggleSelect }></Checkbox>
+                </Grid2>
+                {/* Logo */}
+                <Grid2 size={{xs: 2, md: 1}} sx={{
+                    height: "3rem",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                }}>
+                    <Box component="img" src="/logo192.png" sx={{
+                        maxHeight: "100%",
+                        maxWidth: "100%",
+                    }}></Box>
+                </Grid2>
+            
+                {/* Team Name */}
+                <Grid2 size={{xs: 2, md: 3}}>{team.name}</Grid2>
+            
+                {/* Team Lead */}
+                <Grid2 size={4}>
+                    { (team.teamLead.map((leadId, idx) => {
+                        return (
+                            <Typography key={idx}>{leadId}</Typography>
+                        );
+                    })) }
+                </Grid2>
+                {/* Discord Role */}
+                <Grid2 size={2}>
+                    <Typography>@roleName</Typography>
+                </Grid2>
+            </Grid2>
+        </Paper>
+    );
+}
+
+export default TeamRow;
--- admin-pannel/src/screens/Teams.jsx
+++ admin-pannel/src/screens/Teams.jsx
@@ -1,9 +1,70 @@
+import { useState } from "react";
+import TeamRow from "../components/team-row/teamRow";
 import MainLayout from "../layouts/MainLayout";
+import { Container, Grid2, Stack, Typography } from "@mui/material";
 
 function Teams() {
+    const [selectedTeams, setSelectedTeams] = useState(new Set([]));
+
+    const teamData = [
+        {
+            "id": "wd1",
+            "name": "Web Dev",
+            "teamLead": ["leadUUID"],
+            "foundationDate": "2022-01-01T00:00:00.000Z",
+            "teamLogo": "https://example.com/logo.png",
+            "description": "Team description",
+            "discordId": 123456789012345678,
+            "customDataPublic": {"key": "value"},
+            "customDataPrivate": {"privateKey": "privateValue"},
+            "deletionDate": "2023-09-28T00:00:00.000Z"
+        },
+        {
+            "id": "dd432",
+            "name": "Davis Dot",
+            "teamLead": ["leadUUID"],
+            "foundationDate": "2022-01-01T00:00:00.000Z",
+            "teamLogo": "https://example.com/logo.png",
+            "description": "Team description",
+            "discordId": 123456789012345678,
+            "customDataPublic": {"key": "value"},
+            "customDataPrivate": {"privateKey": "privateValue"},
+            "deletionDate": "2023-09-28T00:00:00.000Z"
+        },
+    ];
+
+    function toggleSelectTeam(teamID) {
+        const newSelected = selectedTeams;
+        if (newSelected.has(teamID)) {
+            newSelected.delete(teamID);
+        } else {
+            newSelected.add(teamID);
+        }
+        setSelectedTeams(newSelected);
+        console.log(newSelected);
+    }
+
     return (
         <MainLayout>
-            Teams
+            <Stack spacing={2}>
+                <Grid2 container spacing={2}>
+                    <Grid2 size={0.5}></Grid2> 
+                    <Grid2 size={4}><Typography variant="h3" sx={{
+                        position: "relative",
+                        left: "1rem",
+                    }}>Team</Typography></Grid2>
+                    <Grid2 size={4}><Typography variant="h3">Team Lead</Typography></Grid2>
+                    <Grid2 size={2}><Typography variant="h3">Discord Role</Typography></Grid2>
+                
+                </Grid2>
+                <Stack spacing={1}>
+                    { teamData.map((team, idx) => {
+                        return (
+                            <TeamRow key={idx} team={team} onToggleSelect={() => toggleSelectTeam(team.id)}></TeamRow>
+                        );
+                    }) }
+                </Stack>
+            </Stack>
         </MainLayout>
     );
 }
